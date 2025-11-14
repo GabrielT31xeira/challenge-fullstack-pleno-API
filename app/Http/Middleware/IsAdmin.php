682950a7ab2a9557712dbab419582a5254b01321
Gabriel Teixeira de Carvalho <gt3ixeira@gmail.com>
2025-11-14 17:38:26 -0300

    Melhoria no middleware de admin

--- app/Http/Middleware/IsAdmin.php
+++ app/Http/Middleware/IsAdmin.php
@@ -4,12 +4,16 @@ namespace App\Http\Middleware;
 
 use Closure;
 use Illuminate\Http\Request;
+use Laravel\Sanctum\PersonalAccessToken;
 
 class IsAdmin
 {
     public function handle(Request $request, Closure $next)
     {
-        if (!$request->user() || !$request->user()->isAdmin()) {
+        $token = $request->bearerToken();
+        $accessToken = PersonalAccessToken::findToken($token);
+
+        if (!$accessToken || !$request->user()->isAdmin()) {
             return response()->json(['message' => 'Acesso negado'], 403);
         }
 
