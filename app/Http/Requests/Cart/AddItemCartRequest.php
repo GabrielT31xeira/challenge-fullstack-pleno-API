98786ef4ca6d5acee7d89ff920b59b47d1535e83
Gabriel Teixeira de Carvalho <gt3ixeira@gmail.com>
2025-11-18 21:16:07 -0300

    Configurações de carrinho adicionais

---
+++ app/Http/Requests/Cart/AddItemCartRequest.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Requests\Cart;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class AddItemCartRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'product_id' => 'required|string|exists:products,id',
+            'quantity' => 'required|integer|min:1'
+        ];
+    }
+
+    public function messages(): array
+    {
+        return [
+            'product_id.required' => 'O campo product_id é obrigatório.',
+            'product_id.string'   => 'O product_id deve ser uma string válida.',
+            'product_id.exists'   => 'O produto informado não foi encontrado.',
+
+            'quantity.required' => 'A quantidade é obrigatória.',
+            'quantity.integer'  => 'A quantidade deve ser um número inteiro.',
+            'quantity.min'      => 'A quantidade mínima permitida é 1.',
+        ];
+    }
+}
