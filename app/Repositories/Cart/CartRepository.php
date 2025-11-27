e592ead677b40305a9e5f5aa8f4e750cbf5de375
Gabriel Teixeira de Carvalho <gt3ixeira@gmail.com>
2025-11-27 11:48:49 -0300

    Pequenas correções de nome no backend

--- app/Repositories/Cart/CartRepository.php
+++ app/Repositories/Cart/CartRepository.php
@@ -21,7 +21,7 @@ class CartRepository implements CartRepositoryInterface
     public function getUserCarts(array $filters, string $userId)
     {
         $query = Cart::where('user_id', $userId)
-            ->whereDoesntHave('orders')
+            ->whereDoesntHave('order')
             ->with('items.product');
         if (!empty($filters['search'])) {
             $query->where(function ($q) use ($filters) {
